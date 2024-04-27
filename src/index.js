465df3a415c0e7144384deed6cc62a3d6b01df69
tertychnyyvs <tertychnyyvs@sibur.ru>
2024-04-27 19:44:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', (req, res) => {
+  const { name } = req.query;
+  res.send(`Hello, ${name}!`);
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
 });
 
 app.listen({ port }, () => {
